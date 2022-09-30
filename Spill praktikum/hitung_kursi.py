29dd31fa1f419553dde5471c512f6d5804dea4af
KaonWry <titan2011official@gmail.com>
2022-09-30 13:41:04 +0700

    spill ngapain aja pas praktikum

---
+++ Spill praktikum/hitung_kursi.py
@@ -0,0 +1,30 @@
+# NIM / Nama : 16322294 / Trystan Prastanov Gabriel
+# Tanggal : 27 September 2022
+# Deskripsi : Program pengatur kursi pesawat
+
+# KAMUS
+# seat : int
+# line : string
+# column : string
+
+# Input data
+seat = int(input('Masukan nomor antrian tiket : '))
+
+# Cari baris
+if (seat % 4 == 0) :
+    line = str(int(seat / 4))
+else :
+    line = str(int(seat / 4) + 1)
+
+# Cari kolom
+if (seat % 4 == 1) :
+    column = 'A'
+elif (seat % 4 == 2) :
+    column = 'B'
+elif (seat % 4 == 3) :
+    column = 'C'
+elif (seat % 4 == 0) :
+    column = 'D'
+
+# Output
+print('Tempat duduk tiket nomor', seat, 'memiliki nomor', line + column)
