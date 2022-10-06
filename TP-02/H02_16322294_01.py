3fe5661b3d801779badbf775cc3598d7ec1e1ec0
KaonWry <titan2011official@gmail.com>
2022-10-06 10:37:18 +0700

    TP 02 prak pengkom nomor 3 blom paham tolooooong

---
+++ TP-02/H02_16322294_01.py
@@ -0,0 +1,21 @@
+# NIM/NAMA  : 16322294 / Trystan Prastanov Gabriel
+# Tanggal   : 5 Oktober 2022
+# Deskripsi : Program mengecek bilangan sempurna
+
+# Input data
+inpNum = int(input('Masukan bilangan: '))
+
+# Cari jumlah faktor
+i =  int(1)
+factorSum = int(0)
+while (i < inpNum) :
+    if (inpNum % i == 0) :
+        factorSum += i
+    print(i, factorSum)
+    i += 1
+
+# Cek kesamaan jumlah faktor dengan bilangan input
+if (factorSum == inpNum) :
+    print('Bilangan tersebut adalah bilangan sempurna')
+else :
+    print('Bilangan tersebut bukan bilangan sempurna')
---
+++ TP-02/H02_16322294_02.py
@@ -0,0 +1,38 @@
+# NIM/NAMA  : 16322294 / Trystan Prastanov Gabriel
+# Tanggal   : 5 Oktober 2022
+# Deskripsi : Program mengecek jumlah dari bilangan yang membesar
+
+# Input 1 pengecualian karena belum ada numPast
+numNow = int(input('Angka ke-1: ')) 
+
+# Init variables
+minCounter = int(0)
+biggerSum = int(0)
+
+# Input data
+i = int(2)
+while True :
+    displayInput = str('Angka ke-' + str(i) + ': ')
+    numNow = int(input(displayInput))
+     
+# Sinpan angka sebelumnya
+    numPast = numNow
+
+# Hitung jumlah kejadian angka sekarang lebih kecil dibanding angka sebelumnya
+    if (numNow <= numPast) :
+        minCounter += 1
+        
+# Reset counter dan jumlahkan angka jika angka membesar 
+    elif (numNow > numPast) :
+        minCounter = 0
+        biggerSum += numNow
+        
+# Keluar loop jika lebih kecil berturut-turut sampai 3 kali
+    if (minCounter == 3) :
+        break
+
+# Add counter
+    i += 1
+    
+# Output data
+print('Jumlah nilai yang membesar adalah ' + str(biggerSum) + '.')
